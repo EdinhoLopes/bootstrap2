5332d969e9c25afe0af314355a53c95f1c4037b3
EdinhoLopes <edsonlopesgomes7@gmail.com>
2023-01-16 16:40:12 -0300

    Create a Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(artcile_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artcile_params
+    params.require(:article).permit(:title, :body)
+  end
 end
